520ef693385071140367350e102278ca4826a65a
Jordon Bedwell <jordon@envygeeks.com>
2012-10-10 06:40:20 -0500

    Make tests fail if other pieces aren't working and add tests for the binary.

--- tests/clippy.rb
+++ tests/clippy.rb
@@ -13,14 +13,42 @@ describe Clippy do subject { Clippy }
 
   describe ".copy" do
     it "must be able to copy" do
+      subject.clear.must_equal(true)
       subject.copy("example").must_equal("example")
+    end
+
+    it "it must copy from the binary" do
+      subject.clear.must_equal(true)
+      data, status = "example", 1
+      Open3.popen3(File.expand_path("../../bin/clippy --copy", __FILE__)) do
+      |stdin, _, _, thread|
+        stdin << data
+        stdin.close
+        status = thread.value
+      end
+
+      status.must_equal(0)
+      subject.paste.must_equal("example")
     end
   end
 
   describe ".paste" do
     it "must be able to paste" do
+      subject.clear.must_equal(true)
       subject.copy("example")
       subject.paste.must_equal("example")
+    end
+
+    it "must be able to paste from the binary" do
+      subject.clear.must_equal(true)
+      data, status = "", 1
+      subject.copy("example").must_equal("example")
+      Open3.popen3(File.expand_path("../../bin/clippy --paste", __FILE__)) {
+      |_, stdout, _, thread|
+        data = stdout.read; status = thread.value }
+
+      status.must_equal(0)
+      data.strip.must_equal("example")
     end
   end
 
