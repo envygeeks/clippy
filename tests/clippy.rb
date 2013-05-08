540a14e23f5e3a8fc97429d535182e1a8dc1b3c3
Jordon Bedwell <envygeeks@gmail.com>
2013-05-08 11:38:53 -0700

    Cleanup the test a bit

--- tests/clippy.rb
+++ tests/clippy.rb
@@ -5,11 +5,6 @@ require "minitest/spec"
 require "clippy"
 
 describe Clippy do subject { Clippy }
-  it "must have a proper version" do
-    Clippy::VERSION.split(".").delete_if { |val|
-      val =~ /pre\d+/ }.length.must_equal(3)
-  end
-
   describe ".copy" do
     it "must be able to copy" do
       subject.clear.must_equal(true)
@@ -42,9 +37,10 @@ describe Clippy do subject { Clippy }
       subject.clear.must_equal(true)
       data, status = "", 1
       subject.copy("example").must_equal("example")
-      Open3.popen3(File.expand_path("../../bin/clippy --paste", __FILE__)) {
-      |_, stdout, _, thread|
-        data = stdout.read; status = thread.value }
+      Open3.popen3(File.expand_path("../../bin/clippy --paste", __FILE__)) do |_, stdout, _, t|
+        data = stdout.read
+        status = t.value
+      end
 
       status.must_equal(0)
       data.strip.must_equal("example")
