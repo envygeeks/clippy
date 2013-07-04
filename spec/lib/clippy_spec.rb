0d4bd51c2affe021ad0b9c3d8aa5a9f9f00c57fd
Jordon Bedwell <envygeeks@gmail.com>
2013-07-04 07:44:05 -0500

    Quotes.

--- spec/lib/clippy_spec.rb
+++ spec/lib/clippy_spec.rb
@@ -1,24 +1,24 @@
-require "rspec/helper"
+require 'rspec/helper'
 
-shared_examples "Clippy" do
-  it "can paste" do
+shared_examples 'Clippy' do
+  it 'can paste' do
     expect(Clippy.copy(str = SecureRandom.hex)).to be_true
     expect(Clippy.paste).to eq str
   end
 
-  it "can copy" do
+  it 'can copy' do
     expect(Clippy.copy(str = SecureRandom.hex)).to be_true
     expect(get_clipboard_contents).to eq str
   end
 
-  it "can clear" do
+  it 'can clear' do
     Clippy.copy(SecureRandom.hex)
     Clippy.clear
     expect(Clippy.paste).to be_nil
   end
 end
 
-describe "Clippy" do
+describe 'Clippy' do
   before :each do
     clear_binary
     Clippy.clear
@@ -27,16 +27,16 @@ describe "Clippy" do
 
   it "sends clip for Windows" do
     Clippy.stub(:windows?).and_return(true)
-    expect(Clippy.binary).to eq "clip"
+    expect(Clippy.binary).to eq 'clip'
   end
 
-  context "with xclip" do
-    it_behaves_like "Clippy"
+  context 'with xclip' do
+    it_behaves_like 'Clippy'
   end
 
-  context "with xsel" do
-    before(:each) { stub_binary("xsel") }
-    it_behaves_like "Clippy"
+  context 'with xsel' do
+    before(:each) { stub_binary('xsel') }
+    it_behaves_like 'Clippy'
   end
 
   it "raises UnknownClipboardError if it can't find the binary" do
--- spec/rspec/helper.rb
+++ spec/rspec/helper.rb
@@ -1,26 +1,26 @@
-unless %W(no false).include?(ENV["COVERAGE"])
+unless %W(no false).include?(ENV['COVERAGE'])
   unless Gem::Specification.find_all_by_name('envygeeks-coveralls').empty?
     require 'envygeeks/coveralls'
   end
 
   SimpleCov.start do
-    add_filter "/spec/"
+    add_filter '/spec/'
   end
 end
 
-require "securerandom"
-require "clippy"
+require 'securerandom'
+require 'clippy'
 
 def get_clipboard_contents
-  out = ""
+  out = ''
 
-  Open3.popen3("xclip -o -selection clipboard") do |stdin, stdout, stderr, pid|
+  Open3.popen3('xclip -o -selection clipboard') do |stdin, stdout, stderr, pid|
     out = stdout.read.strip
   end
 out
 end
 
-def stub_binary(with = "xclip")
+def stub_binary(with = 'xclip')
   clear_binary
   Clippy.send(:instance_variable_set, :@binary, with)
 end
