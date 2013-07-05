787e4ef2ed137b59c7f7cda14ab35e422a6f068b
Jordon Bedwell <envygeeks@gmail.com>
2013-07-05 14:28:57 -0500

    Make luna-rspec-formatters optional.

--- spec/rspec/helper.rb
+++ spec/rspec/helper.rb
@@ -28,3 +28,10 @@ end
 def clear_binary
   Clippy.send(:remove_instance_variable, :@binary) if Clippy.instance_variable_defined?(:@binary)
 end
+
+RSpec.configure do |config|
+  if Gem::Specification.find_all_by_name('luna-rspec-formatters').any?
+    require 'luna/rspec/formatters/checks'
+    config.formatter = 'Luna::RSpec::Formatters::Checks'
+  end
+end
