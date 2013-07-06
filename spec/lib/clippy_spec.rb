cfab9da5930312015be82821c70fbd0bc99c32fc
Jordon Bedwell <envygeeks@gmail.com>
2013-07-05 19:36:55 -0500

    Increase coverage.

--- spec/lib/clippy_spec.rb
+++ spec/lib/clippy_spec.rb
@@ -30,6 +30,34 @@ describe 'Clippy' do
     expect(Clippy.binary).to eq 'clip'
   end
 
+  describe '#paste' do
+    before :each do
+      unless Clippy.windows?
+        class Win32API
+          def initialize(*args)
+            # Mocked
+          end
+
+          def call(*args)
+            return args
+          end
+        end
+      end
+    end
+
+    after :each do
+      unless Clippy.windows?
+        Object.send(:const_remove, Win32API)
+      end
+    end
+
+    it 'calls Win32API for Windows' do
+      Win32API.should_receive(:new).exactly(3).times.and_call_original
+      Clippy.stub(:windows?).and_return(true)
+      Clippy.paste
+    end
+  end
+
   context 'with xclip' do
     it_behaves_like 'Clippy'
   end
@@ -37,6 +65,24 @@ describe 'Clippy' do
   context 'with xsel' do
     before(:each) { stub_binary('xsel') }
     it_behaves_like 'Clippy'
+  end
+
+  describe '#binary' do
+    it 'gives xclip preferably' do
+      expect(Clippy.binary).to eq 'xclip'
+    end
+
+    it 'gives xsel' do
+      Clippy.should_receive(:system).ordered.and_return(false)
+      Clippy.should_receive(:system).ordered.and_return(true)
+      expect(Clippy.binary).to eq 'xsel'
+    end
+
+    it 'gives pbcopy on Mac' do
+      Clippy.should_receive(:system).twice.ordered.and_return(false)
+      Clippy.should_receive(:system).and_return(true)
+      expect(Clippy.binary).to eq 'pbcopy'
+    end
   end
 
   it "raises UnknownClipboardError if it can't find the binary" do
